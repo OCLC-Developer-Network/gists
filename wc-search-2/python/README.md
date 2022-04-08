aafa73cd4266eb35a5a2471a31ee09c09110ce98
Karen Coombs <coombsk@oclc.org>
2022-04-08 13:23:10 -0500

    Added example for wc-search-v2

---
+++ wc-search-2/python/README.md
@@ -0,0 +1,16 @@
+# gists
+## Python
+### Install pip
+https://pip.pypa.io/en/stable/installing/
+### Install dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+### Run the examples
+getTitles.py can be run from the command line using
+
+```bash
+python getTitles.py
+```
---
+++ wc-search-2/python/getTitles.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ###############################################################################
+# Copyright 2014 OCLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# Example of retrieving a token with Client Credentials Grant and https://github.com/requests/requests-oauthlib
+
+import yaml
+from oauthlib.oauth2 import BackendApplicationClient
+from oauthlib.oauth2 import TokenExpiredError
+from requests.auth import HTTPBasicAuth
+from requests_oauthlib import OAuth2Session
+import requests
+ 
+with open("../config.yml", 'r') as stream:
+    config = yaml.safe_load(stream)
+     
+serviceURL = config.get('discovery_service_url')   
+# get a token
+scope = ['wcapi']
+auth = HTTPBasicAuth(config.get('key'), config.get('secret'))
+client = BackendApplicationClient(client_id=config.get('key'), scope=scope)
+wskey = OAuth2Session(client=client)
+
+oclcNumbers = [1, 935945992, 856932916, 856869292, 864903954, 957589983]
+try:
+    token = wskey.fetch_token(token_url=config.get('token_url'), auth=auth)
+    for oclcNumber in oclcNumbers: 
+        try:
+            r = wskey.get(serviceURL + "/brief-bibs/" + str(oclcNumber))
+            r.raise_for_status()
+            result = r.json()
+            print(result.get('title'))
+        except TokenExpiredError as err: 
+          token = wskey.fetch_token(token_url=config.get('token_url'), auth=auth) 
+          r = wskey.get(serviceURL + "/brief-bibs/" + str(oclcNumber))
+          result = r.json()
+          print(result.get('title'))
+        except requests.exceptions.HTTPError as err:
+            print(err)
+    
+        
+except BaseException as err:
+    print(err)       
