d6a0894b91846df9d78c709f368169aaccb468d0
Karen Coombs <coombsk@oclc.org>
2025-06-04 13:16:20 -0400

    Added more fields to deleteLHRs output spreadsheet

--- wc-metadata/python/deleteLHRs.py
+++ wc-metadata/python/deleteLHRs.py
@@ -11,8 +11,6 @@ with open("config.yml", 'r') as stream:
 
 scope = ['WorldCatMetadataAPI:view_my_holdings', 'WorldCatMetadataAPI:manage_institution_lhrs']
 
-
-
 auth = HTTPBasicAuth(config.get('key'), config.get('secret'))
 client = BackendApplicationClient(client_id=config.get('key'), scope=scope)
 oauth_session = OAuth2Session(client=client)
@@ -43,28 +41,42 @@ def searchLHR(identifier, type, holdingLocation):
                 if result.get('numberOfHoldings') == 1:
                     lhrID = result.get('detailedHoldings')[0].get('lhrControlNumber')
                     status = deleteLHR(lhrID)
+                    barcode = result.get('detailedHoldings')[0].get('lhrControlNumber').get("holdingParts").get("pieceDesignation")
                 elif result.get('numberOfHoldings') > 1:
                     if holdingLocation:
                         holdings = result.get('detailedHoldings')
                         lhr = [holding for holding in holdings if holding.get("location").get('sublocationCollection') == holdingLocation]
                         if len(lhr) > 1:
                             status = "failed more than 1 LHR matched"
+                            lhrID = ""
+                            barcode = ""
                         else:
                             lhrID = lhr[0].get('lhrControlNumber')
                             status = deleteLHR(lhrID)
+                            barcode = ""
                     else:
                         status = "failed more than 1 LHR matched"
+                        lhrID = ""
+                        barcode = ""
                 elif result.get('numberOfHoldings') > 1:
                     status = "failed more than 1 LHR matched"
+                    lhrID = ""
+                    barcode = ""
                 else:
                     status = "failed no LHRs matched"
+                    lhrID = ""
+                    barcode = ""
             except json.decoder.JSONDecodeError:
                 status = 'failed'
+                lhrID = ""
+                barcode = ""
         except requests.exceptions.HTTPError as err:
             status = "failed"
+            lhrID = ""
+            barcode = ""
     else:
         raise ValueError('Invalid type value. Valid type values are' + ','.join(str(identifiers.keys)))
-    return status
+    return pd.Series([status, lhrID, barcode])
 
 
 def deleteLHR(lhrID):
@@ -87,7 +99,7 @@ csv_read = pd.read_csv(file, index_col=False, encoding='utf-8', nrows=25000)
 
 try:
     token = oauth_session.fetch_token(token_url="https://oauth.oclc.org/token", auth=auth)
-    csv_read[['status']] = csv_read.apply (lambda row: removeLHR(row['oclcNumber'], 'oclcNumber'), axis=1)
+    csv_read[['status', 'lhr', 'barcode']] = csv_read.apply (lambda row: removeLHR(row['oclcNumber'], 'oclcNumber'), axis=1)
 
     output_dir = "getMetadata-result.csv"
     csv_read.to_csv(output_dir, index=False)
